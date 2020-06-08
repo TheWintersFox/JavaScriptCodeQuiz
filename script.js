263474489eda9492a44328022f65edb24806f162
Luke Evans <lukeevanstax@gmail.com>
2020-06-07 21:33:48 -0400

    updated setStatusClass function when correct and wrong, added Clear status Class to remove correct and wrong

--- script.js
+++ script.js
@@ -60,5 +60,24 @@ function resetState() {
 }
 
 function selectAnswer() {
+    const selectedButton = e.traget
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+}
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
