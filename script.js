06133265fe649c14e3206d11199fea6743ec7d7b
Luke Evans <lukeevanstax@gmail.com>
2020-06-07 21:17:42 -0400

    adding Javascript update with random question shuffling, question generation w/ true boolean and false boolean, setting up functions for start game, next question, show question, and select answer functions

---
+++ script.js
@@ -0,0 +1,43 @@
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+
+let shuffledQuestions, currentQuestionIndex
+
+
+startButton.addEventListener('click', startGame)
+
+
+
+function startGame() {
+    console.log('Started');
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion()
+
+}
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+    questionElement.innerText = question.question
+}
+
+function showQuestion(question) {
+
+}
+
+function selectAnswer() {
+
+}
+
+const questions = [{
+    question: 'What is 2 + 2',
+    answers: [
+        { text: '4', correct: true },
+        { text: '22', correct: false }
+    ]
+}]
