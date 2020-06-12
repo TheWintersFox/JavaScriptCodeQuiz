a61d02e97e52c25cb32df058c8810741994a5494
Luke Evans <lukeevanstax@gmail.com>
2020-06-12 01:37:23 -0400

    adding back start game function with Next question shuffle

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //HTML elements
-const startButton = document.getElementById('start-btn')
+const startButton = document.getElementById('startBtn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
@@ -10,69 +10,16 @@ let shuffledQuestions, currentQuestionIndex
 let secondsLeft = 75;
 
 
-// Questions Array
-const questions = [{
-    question: 'What is the model of the first broom Harry ever receives?',
-    answers: [
-         'Nimbus 2000',
-         'Cleansweep One',
-         'Hoover',
-         'Firebolt',
-    ],
-    correctAnswer: 'Nimbus 2000',
-},
-{
-    question: 'How does Harry manage to breathe underwater during the second task of the Triwizard Tournament?',
-    answers: [
-         'He transfigures into a shark',
-          'He kisses a mermaid',
-         'He eats gillyweed',
-         'He performs a bubble-head charm',
-    ],
-    correctAnswer: 'He eats gillyweed'
-},
-{
-    question: 'What is the name of Fred and George’s joke shop?',
-    answers: [
-         'Weasley Joke Emporium',
-         'Weasleys Wizard Wheezes',
-         'Fred & Georges Wonder Emporium',
-         'Zonkos Joke Shop'
-    ],
-    correctAnswer: 'Weasleys Wizard Wheezes',
-},
-{
-    question: 'Which of these is NOT one of the Unforgivable Curses?',
-    answers: [
-        'Cruciatus Curse', 
-        'Imperius Curse', 
-        'Sectumsempra', 
-        'Avada Kedavra',
-    ],
-    correctAnswer: 'Sectumsempra',
-},
-{
-    question: 'Where does Hermione brew her first batch of Polyjuice Potion?',
-    answers: [
-        'Moaning Myrtle’s Bathroom',
-        'The Hogwarts Kitchen',
-        'The Room of Requirement',
-        'The Gryffindor Common Room',   
-         
-    ],
-    correctAnswer: 'Moaning Myrtle’s Bathroom',  
-}
-]
-
 //START BUTTON
 startButton.addEventListener('click', function (event) {
     currentQuestionIndex++
     event.preventDefault();
 
+    //Hide Start Display
+    startQuizz.style.display = "none";
+
     //Start Timer
     startTimer();
-
-    startQuiz.style.display = "none";
 
     //Start Questions
     startGame();
@@ -112,7 +59,6 @@ function startGame() {
     questionContainerElement.classList.remove('hide');
     h1TextElement.innerText = ""
     displayNextQuestion()
-
 }
 
 function displayNextQuestion() {
@@ -120,3 +66,59 @@ function displayNextQuestion() {
     showQuestion(shuffledQuestions[currentQuestionIndex])
 
 }
+
+
+
+// Questions Array
+const questions = [{
+    question: 'What is the model of the first broom Harry ever receives?',
+    answers: [
+     'Nimbus 2000',
+     'Cleansweep One',
+     'Hoover',
+     'Firebolt',
+    ],
+    correctAnswer: 'Nimbus 2000',
+},
+{
+    question: 'How does Harry manage to breathe underwater during the second task of the Triwizard Tournament?',
+    answers: [
+     'He transfigures into a shark',
+     'He kisses a mermaid',
+     'He eats gillyweed',
+     'He performs a bubble-head charm',
+    ],
+    correctAnswer: 'He eats gillyweed'
+},
+{
+    question: 'What is the name of Fred and George’s joke shop?',
+    answers: [
+     'Weasley Joke Emporium',
+     'Weasleys Wizard Wheezes',
+     'Fred & Georges Wonder Emporium',
+     'Zonkos Joke Shop'
+    ],
+    correctAnswer: 'Weasleys Wizard Wheezes',
+},
+{
+    question: 'Which of these is NOT one of the Unforgivable Curses?',
+    answers: [
+     'Cruciatus Curse', 
+     'Imperius Curse', 
+     'Sectumsempra', 
+     'Avada Kedavra',
+    ],
+    correctAnswer: 'Sectumsempra',
+},
+{
+    question: 'Where does Hermione brew her first batch of Polyjuice Potion?',
+    answers: [
+     'Moaning Myrtle’s Bathroom',
+     'The Hogwarts Kitchen',
+     'The Room of Requirement',
+     'The Gryffindor Common Room',   
+         
+    ],
+    correctAnswer: 'Moaning Myrtle’s Bathroom',  
+}
+]
