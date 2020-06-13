77b7b11ab7ff07f38907b585287ad32d9541d965
Luke Evans <lukeevanstax@gmail.com>
2020-06-12 23:43:47 -0400

    draft of highscore pages, per code demonstrated in office hours

--- script.js
+++ script.js
@@ -6,11 +6,26 @@ const answerButtonsElement = document.getElementById('answer-buttons');
 const h1TextElement = document.getElementById('H1');
 
 
-//Random Shuffling of questions
-let shuffledQuestions;
-let secondsLeft = 75;
-let currentQuestionIndex = 0;
+
+let shuffledQuestions; //Random Shuffling of questions
+
+let logScore = " ";
+let highscore = [];
+let username = " ";
+let user = [];
+let scoreArray = localStorage.getItem('highscore');
+if (scoreArray) {
+    scoreArray = JSON.parse(scoreArray);
+    // else {
+    //     scoreArray = [];
+    // }
+}
+
+
+let secondsLeft = 75; //Starting time
+let currentQuestionIndex = 0; // Starting index at 0
 let checkAnswerDisplay = document.createElement('p');
+
 
 
 // Questions Array
@@ -104,6 +119,12 @@ function startTimer() {
     let timerInterval = setInterval(time, 1000);
 }
 
+//STOP TIMER Function
+function stopTimer() {
+    clearInterval(logScore);
+    timer.timerContent = '';
+}
+
 
 //Start Game Function w/ Question array random shuffle
 function startGame() {
@@ -182,7 +203,7 @@ function setStatusClass(element, correct) {
 //This will check the selected choice then display correct or incorrect below the answers area.
 function checkAnswer(response) {
     console.log(response);
-    if (response !== questions[currentQuestionIndex].correctAnswer) {
+    if (response !== questions[currentQuestionIndex]) {
         console.log("WRONG");
         checkAnswerDisplay.textContent = "Incorrect";
         checkAnswerDisplay.style.textAlign = "center";
@@ -214,8 +235,136 @@ function resetState() {
     }
 }
 
-
 // EndQUIZ WIP
-function endQuizPlaceHolder() {
-    alert("Game Over");
+function endQuiz() {
+    //Function stops timer once time is logged as the score.
+    stopTimer();
+
+    /* Hide quiz instructions and start button as well as the questions and answers 
+    - including the incorrect or correct display at the buttom of the page*/
+    quizHeader.style.display = "none";
+    startDisplay.style.display = "none";
+    qa.style.display = "none";
+    answerCheck.style.display = "none";
+
+    //HIGHSCORE LOCAL VARIABLES
+    /*Description: Variables needed in order to collect and store highscores. Creates Initial Input Display.*/
+    const highscoreInputHeader = document.createElement("h1");
+    const highscoreInputHeaderText = document.createTextNode("Highscores");
+    let score = secondsLeft; //Score calculated by determining the amount of time left when the quiz ends.
+    const scoreDisplay = document.createElement("p");
+    scoreDisplay.textContent = "Your final score is " + score + ".";
+    const initialsInputLabel = document.createElement("LABEL");
+    initialsInputLabel.textContent = "Enter initials: ";
+    initialsInputLabel.className += "mr-1"
+    const initialsInput = document.createElement("INPUT");
+    initialsInput.setAttribute("type", "text");
+    initialsInput.setAttribute("value", "SLH");
+    const submitButton = document.createElement("button");
+    submitButton.innerHTML = "Submit";
+    submitButton.className += "btn btn-primary ml-1";
+
+    //SUBMIT HIGHSCORE
+    /*Description: Upon clicking the submit button, the username and score is saved 
+    to the localStorage and displayed under Highscores (which can also be accessed by pressing the
+    'View Highscores' button shown below). */
+    submitButton.onclick = function(event) {
+        event.preventDefault;
+        //Prompt for initials to collect username for localStorage
+        let userName = initialsInput.value;
+        console.log(userName);
+        //STORE SCORE
+        function storeHighscore() {
+            //localStorage.getItem('highscore')
+            // if the above is a value (scoreArray) push new object ie. { user: userName, score: score }
+            scoreArray.push({ user: userName, score: score });
+            localStorage.setItem('highscore', JSON.stringify(scoreArray))
+                // scoreArray = [{ user: userName, score: score },  { user: userName, score: score }]
+        }
+        //STORE USER
+        // function storeUser() {
+        //   localStorage.setItem("user", JSON.stringify(userName));
+        // }
+        //LOGGED FUNCTION 
+        /*Description: The username and score are stored IF the current score 
+        is higher than the previously stored highscore*/
+        // if (score > JSON.parse(localStorage.getItem("highscore"))) {
+        storeHighscore();
+        // }
+        //Function creates input area
+        highscoresDisplay();
+    };
+
+    //Displays Highscore Input Area in HTML
+    highscoreInputHeader.appendChild(highscoreInputHeaderText);
+    highscoreContent.appendChild(highscoreInputHeader);
+    highscoreContent.appendChild(scoreDisplay);
+    highscoreContent.appendChild(initialsInputLabel);
+    highscoreContent.appendChild(initialsInput);
+    highscoreContent.appendChild(submitButton);
+
+}
+
+//VIEW HIGHSCORE BUTTON
+const highscoreButton = document.getElementById("highscorebtn");
+highscoreButton.onclick = function(event) {
+    event.preventDefault;
+    answerCheck.style.display = "none";
+    stopTimer();
+    highscoresDisplay();
+};
+
+//HIGHSCORE DISPLAY FUNCTION
+/*Inputs are displayed after inputs are collected. Intials and highscore displayed. Score displayed is equivalent to 
+the seconds left on the timer when the quiz ends. */
+function highscoresDisplay() {
+    /* Hide quiz instructions and start button as well as the questions and answers 
+   and the Highscore input diplay page*/
+    quizHeader.style.display = "none";
+    startDisplay.style.display = "none";
+    qa.style.display = "none";
+    highscoreContent.style.display = "none";
+    //Creates "Highscore" Header and collects data for localStorage
+    const highscoreHeader = document.createElement("h1");
+    const highscoreHeaderText = document.createTextNode("Highscores");
+    const highscoreList = document.createElement("div");
+    highscoreHeader.appendChild(highscoreHeaderText);
+    highscoreDisplay.appendChild(highscoreHeader);
+    for (let i = 0; i < scoreArray.length; i++) {
+        let highscoreLatest = document.createElement("p");
+        let highscoreLatestText = document.createTextNode(
+            scoreArray[i].user +
+            " - " +
+            scoreArray[i].score
+        );
+        highscoreLatest.appendChild(highscoreLatestText);
+        highscoreList.appendChild(highscoreLatest);
+    };
+    highscoreDisplay.appendChild(highscoreList);
+    //GO BACK BUTTON
+    const goBackButton = document.createElement("button");
+    goBackButton.innerHTML = "Go Back";
+    goBackButton.className += "btn btn-primary mr-3";
+    goBackButton.onclick = function(event) {
+        event.preventDefault;
+        window.location.reload();
+    };
+
+    //CLEAR BUTTON
+    const clearButton = document.createElement("button");
+    clearButton.innerHTML = "Clear Highscore";
+    clearButton.className += "btn btn-primary";
+    clearButton.onclick = function(event) {
+        event.preventDefault;
+        window.localStorage.clear();
+        highscoreList.innerHTML = '';
+
+    };
+
+    //Displays highscore information
+    highscoreDisplay.appendChild(goBackButton);
+    highscoreDisplay.appendChild(clearButton);
+
+    //Disable View Highscore Button
+    highscoreButton.disabled = true;
 }
